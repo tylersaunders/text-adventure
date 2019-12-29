e1e619011061563f465274c0d2bdfc9033dfebc2
Tyler Saunders <47871226+tylersaunders@users.noreply.github.com>
2019-12-28 18:28:20 -0800

    Initial Webpack setup

---
+++ app/main.js
@@ -0,0 +1,5 @@
+import './styles.scss';
+
+console.log('hello world');
+let test = 'hello world this is es6';
+console.log(test);
---
+++ config/webpack.config.js
@@ -0,0 +1,57 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: './app/main.js',
+  output: {
+    path: path.resolve(__dirname, '../dist'),
+    filename: 'bundle.js',
+  },
+
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          {
+            // After all CSS loaders we use plugin to do his work.
+            // It gets all transformed CSS and extracts it into separate
+            // single bundled file
+            loader: MiniCssExtractPlugin.loader,
+          },
+          {
+            // This loader resolves url() and @imports inside CSS
+            loader: 'css-loader',
+          },
+          {
+            // Then we apply post CSS fixes like autoprefixer and minifying
+            loader: 'postcss-loader',
+            options: {config: {path: 'config/postcss.config.js'}},
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              implementation: require('sass'),
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'bundle.css',
+    }),
+  ],
+};
