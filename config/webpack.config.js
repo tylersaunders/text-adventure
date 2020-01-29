2945788f5bca8a30c6b071503d9d2436482492b3
Tyler Saunders <47871226+tylersaunders@users.noreply.github.com>
2020-01-28 21:30:38 -0800

    Webpack fixes to emit css without having to import it into js

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -2,56 +2,58 @@ const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  entry: './web/main.js',
-  output: {
-    path: path.resolve(__dirname, '../build'),
-    filename: 'bundle.js',
+  entry : {
+    app : [ './web/main.js', './web/styles.scss' ],
+  },
+  output : {
+    path : path.resolve(__dirname, '../build'),
+    filename : 'bundle.js',
   },
 
-  mode: 'development',
-  module: {
-    rules: [
+  mode : 'development',
+  module : {
+    rules : [
       {
-        test: /\.js$/,
-        exclude: /(node_modules)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
+        test : /\.js$/,
+        exclude : /(node_modules)/,
+        use : {
+          loader : 'babel-loader',
+          options : {
+            presets : [ '@babel/preset-env' ],
           },
         },
       },
       {
-        test: /\.(sa|sc|c)ss$/,
-        use: [
+        test : /\.(sa|sc|c)ss$/,
+        use : [
           {
             // After all CSS loaders we use plugin to do his work.
             // It gets all transformed CSS and extracts it into separate
             // single bundled file
-            loader: MiniCssExtractPlugin.loader,
+            loader : MiniCssExtractPlugin.loader,
           },
           {
             // This loader resolves url() and @imports inside CSS
-            loader: 'css-loader',
+            loader : 'css-loader',
           },
           {
             // Then we apply post CSS fixes like autoprefixer and minifying
-            loader: 'postcss-loader',
-            options: {config: {path: 'config/postcss.config.js'}},
+            loader : 'postcss-loader',
+            options : {config : {path : 'config/postcss.config.js'}},
           },
           {
-            loader: 'sass-loader',
-            options: {
-              implementation: require('sass'),
+            loader : 'sass-loader',
+            options : {
+              implementation : require('sass'),
             },
           },
         ],
       },
     ],
   },
-  plugins: [
+  plugins : [
     new MiniCssExtractPlugin({
-      filename: 'bundle.css',
+      filename : 'bundle.css',
     }),
   ],
 };
--- web/main.js
+++ web/main.js
@@ -1,5 +1,1 @@
-import './styles.scss';
-
-console.log('hello world');
-let test = 'hello world this is es6';
-console.log(test);
+// import './styles.scss';
